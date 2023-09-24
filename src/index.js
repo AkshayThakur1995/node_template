f858f965fd78c862490a53608b70188455db4247
AkshayThakur1995 <akshaymarx08@gmail.com>
2023-09-24 13:30:45 +0530

    base node project template

--- src/index.js
+++ src/index.js
@@ -8,4 +8,6 @@ app.use("/api", apiRoutes);
 app.listen(ServerConfig.PORT, () => {
   console.log(`server running on ${ServerConfig.PORT}`);
   //   Logger.info("Successfuly started the server", "root", {});
+  // Logger.info("Server started", "root");
+  
 });
